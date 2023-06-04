40e1db9f558f632211ebbbb10c747345dcb689fd
Arlosaid <adroal08@gmail.com>
2023-06-03 19:41:30 -0600

    Adding user and super user

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'restaurant_main'
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -87,6 +87,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
